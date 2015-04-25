0c5b41d5100bd93f1386220ab6766a08ca7f81f8
MuhammadAbdurrahman <muhammadabdurrahman88@yahoo.com>
2015-04-25 18:25:13 +0700

    ini

---
+++ TB/src/KeamananRuangKelas.java
@@ -0,0 +1,29 @@
+
+public class KeamananRuangKelas {
+	String kekokohan, kuncipintudanjendela, bahaya;
+
+	public String getKekokohan() {
+		return kekokohan;
+	}
+
+	public void setKekokohan(String kekokohan) {
+		this.kekokohan = kekokohan;
+	}
+
+	public String getKuncipintudanjendela() {
+		return kuncipintudanjendela;
+	}
+
+	public void setKuncipintudanjendela(String kuncipintudanjendela) {
+		this.kuncipintudanjendela = kuncipintudanjendela;
+	}
+
+	public String getBahaya() {
+		return bahaya;
+	}
+
+	public void setBahaya(String bahaya) {
+		this.bahaya = bahaya;
+	}
+
+}
