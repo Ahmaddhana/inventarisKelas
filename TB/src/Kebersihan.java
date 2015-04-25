b5120ef97b557048b11484d14f6052611509e093
el-mubarok <mubarok13@gmail.com>
2015-04-25 18:57:01 +0700

    Kebersihan & Lingkungan

---
+++ TB/src/Kebersihan.java
@@ -0,0 +1,31 @@
+
+public class Kebersihan {
+
+	private String SirkulUdara;
+	private double NilaiCahaya, Kelembapan, Suhu;
+	
+	public String getSirkulUdara() {
+		return SirkulUdara;
+	}
+	public void setSirkulUdara(String sirkulUdara) {
+		SirkulUdara = sirkulUdara;
+	}
+	public double getNilaiCahaya() {
+		return NilaiCahaya;
+	}
+	public void setNilaiCahaya(double nilaiCahaya) {
+		NilaiCahaya = nilaiCahaya;
+	}
+	public double getKelembapan() {
+		return Kelembapan;
+	}
+	public void setKelembapan(double kelembapan) {
+		Kelembapan = kelembapan;
+	}
+	public double getSuhu() {
+		return Suhu;
+	}
+	public void setSuhu(double suhu) {
+		Suhu = suhu;
+	}
+}
