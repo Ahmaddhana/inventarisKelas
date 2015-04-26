7312811dabceb3baf327bde1a2994e6175574503
el-mubarok <mubarok13@gmail.com>
2015-04-26 18:06:25 +0700

    update control3

--- TB/src/mainprog.java
+++ TB/src/mainprog.java
@@ -4,8 +4,11 @@ public class mainprog {
 	public static void main(String args[]) {
 	
 		control data = new control();
+		control3 data3 = new control3();
 		
 		data.pilihan();
 		data.View();
+		
+		data3.masukan();
 	}
 }
