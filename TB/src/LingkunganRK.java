a5044118adcb625c5c463c1736cc2355c3ecbe29
el-mubarok <mubarok13@gmail.com>
2015-04-26 18:48:02 +0700

    update

---
+++ TB/src/LingkunganRK.java
@@ -0,0 +1,36 @@
+
+public class LingkunganRK {
+
+	private String KondLantai, KonDinding, KonAtap, KonPintu, KonJendela;
+	
+	public String getKondLantai() {
+		return KondLantai;
+	}
+	public void setKondLantai(String kondLantai) {
+		KondLantai = kondLantai;
+	}
+	public String getKonDinding() {
+		return KonDinding;
+	}
+	public void setKonDinding(String konDinding) {
+		KonDinding = konDinding;
+	}
+	public String getKonAtap() {
+		return KonAtap;
+	}
+	public void setKonAtap(String konAtap) {
+		KonAtap = konAtap;
+	}
+	public String getKonPintu() {
+		return KonPintu;
+	}
+	public void setKonPintu(String konPintu) {
+		KonPintu = konPintu;
+	}
+	public String getKonJendela() {
+		return KonJendela;
+	}
+	public void setKonJendela(String konJendela) {
+		KonJendela = konJendela;
+	}
+}
