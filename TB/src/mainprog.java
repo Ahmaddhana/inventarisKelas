98a01d1c937e577bd40428b1ff522c7c9e6e2078
Ahmad Dhana <dhanarenomius@gmail.com>
2015-04-26 21:23:47 +0700

    update

--- TB/src/mainprog.java
+++ TB/src/mainprog.java
@@ -4,12 +4,24 @@ public class mainprog {
 	public static void main(String args[]) {
 	
 		control data1 = new control();
-		control3 data2 = new control3();
+		control2 data2 = new control2();
+		control3 data3 = new control3();
 		
 		data1.masukan();
 		data2.masukan();
+		data3.masukan();
 		
 		data1.View();
-		data2.view();
+		
+		data2.hitungLuas();
+		data2.AnalisisKelistrikan();
+		data2.AnalisisLCD();
+		data2.AnalisisLampu();
+		data2.AnalisisKipasAngin();
+		data2.AnalisisAC();
+		data2.AnalisisInternet();
+		data2.AnalisisCCTV();
+	
+		data3.view();
 	}
 }
