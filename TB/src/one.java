0d76bb3ccdc26a916bab92c33241c218cf515a0a
MuhammadAbdurrahman <muhammadabdurrahman88@yahoo.com>
2015-06-07 22:21:47 +0700

    @actionlistener

--- TB/src/one.java
+++ TB/src/one.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
@@ -17,7 +18,9 @@ import javax.swing.DefaultComboBoxModel;
 public class one extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
+	identitasRuangKelas data = new identitasRuangKelas(); 
 	/**
 	 * Launch the application.
 	 */
@@ -51,44 +54,44 @@ public class one extends JFrame {
 		contentPane.add(btnNext);
 		btnNext.addActionListener(new ActionListener() {
 			
-			@Override
 			public void actionPerformed(ActionEvent e) {
+				data.setNamaR(textField.getText());
 				new Two().setVisible(true);
 				dispose();
 			}
 		});
 		
 		JLabel lblNewLabel = new JLabel("Nama Ruang");
-		lblNewLabel.setBounds(10, 11, 69, 23);
+		lblNewLabel.setBounds(10, 11, 96, 23);
 		contentPane.add(lblNewLabel);
 		
-		JTextPane txtpnNamaRuang = new JTextPane();
-		txtpnNamaRuang.setBounds(89, 11, 113, 23);
-		contentPane.add(txtpnNamaRuang);
+		textField = new JTextField();
+		textField.setBounds(184, 12, 163, 20);
+		contentPane.add(textField);
 		
 		JLabel lblNewLabel_1 = new JLabel("Lokasi ");
 		lblNewLabel_1.setBounds(10, 45, 46, 23);
 		contentPane.add(lblNewLabel_1);
 		
 		JRadioButton rdbtnGkb = new JRadioButton("GKB 1");
-		rdbtnGkb.setBounds(89, 45, 53, 23);
+		rdbtnGkb.setBounds(145, 45, 85, 23);
 		contentPane.add(rdbtnGkb);
 		
 		JRadioButton rdbtnGkb_1 = new JRadioButton("GKB 2");
-		rdbtnGkb_1.setBounds(144, 45, 53, 23);
+		rdbtnGkb_1.setBounds(232, 45, 74, 23);
 		contentPane.add(rdbtnGkb_1);
 		
 		JRadioButton rdbtnGkb_2 = new JRadioButton("GKB 3");
-		rdbtnGkb_2.setBounds(199, 45, 53, 23);
+		rdbtnGkb_2.setBounds(308, 45, 69, 23);
 		contentPane.add(rdbtnGkb_2);
 		
 		JLabel lblProgramStudi = new JLabel("Program Studi");
-		lblProgramStudi.setBounds(10, 79, 69, 23);
+		lblProgramStudi.setBounds(10, 79, 96, 23);
 		contentPane.add(lblProgramStudi);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"ahmad", "dhana", "renomi"}));
-		comboBox.setBounds(89, 80, 113, 20);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Informatika", "Sipil", "Industri", "Elektro", "Mesin"}));
+		comboBox.setBounds(216, 80, 113, 20);
 		contentPane.add(comboBox);
 	}
 }
