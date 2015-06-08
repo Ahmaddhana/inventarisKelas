7d942a497cb203a2ff65b8152df4959d0cde8f01
MuhammadAbdurrahman <muhammadabdurrahman88@yahoo.com>
2015-06-08 11:25:25 +0700

    #newinterface

--- TB/src/one.java
+++ TB/src/one.java
@@ -1,26 +1,23 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
 
 
-public class one extends JFrame {
+public class NomNom extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 
-	identitasRuangKelas data = new identitasRuangKelas(); 
 	/**
 	 * Launch the application.
 	 */
@@ -40,7 +37,7 @@ public class one extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public one() {
+	public NomNom() {
 		setTitle("Identitas Ruang Kelas");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 219);
@@ -52,46 +49,32 @@ public class one extends JFrame {
 		JButton btnNext = new JButton("Next");
 		btnNext.setBounds(335, 146, 89, 23);
 		contentPane.add(btnNext);
-		btnNext.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e) {
-				data.setNamaR(textField.getText());
-				new Two().setVisible(true);
-				dispose();
-			}
-		});
 		
 		JLabel lblNewLabel = new JLabel("Nama Ruang");
 		lblNewLabel.setBounds(10, 11, 96, 23);
 		contentPane.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(184, 12, 163, 20);
-		contentPane.add(textField);
-		
 		JLabel lblNewLabel_1 = new JLabel("Lokasi ");
 		lblNewLabel_1.setBounds(10, 45, 46, 23);
 		contentPane.add(lblNewLabel_1);
-		
-		JRadioButton rdbtnGkb = new JRadioButton("GKB 1");
-		rdbtnGkb.setBounds(145, 45, 85, 23);
-		contentPane.add(rdbtnGkb);
-		
-		JRadioButton rdbtnGkb_1 = new JRadioButton("GKB 2");
-		rdbtnGkb_1.setBounds(232, 45, 74, 23);
-		contentPane.add(rdbtnGkb_1);
-		
-		JRadioButton rdbtnGkb_2 = new JRadioButton("GKB 3");
-		rdbtnGkb_2.setBounds(308, 45, 69, 23);
-		contentPane.add(rdbtnGkb_2);
 		
 		JLabel lblProgramStudi = new JLabel("Program Studi");
 		lblProgramStudi.setBounds(10, 79, 96, 23);
 		contentPane.add(lblProgramStudi);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Informatika", "Sipil", "Industri", "Elektro", "Mesin"}));
-		comboBox.setBounds(216, 80, 113, 20);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"INFORMATIKA", "SIPIL", "INDUSTRI", "ELEKTRI", "MESIN"}));
+		comboBox.setBounds(184, 80, 163, 20);
 		contentPane.add(comboBox);
+		
+		textField = new JTextField();
+		textField.setBounds(184, 12, 163, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"GKB 1", "GKB 2", "GKB 3"}));
+		comboBox_1.setBounds(184, 46, 163, 20);
+		contentPane.add(comboBox_1);
 	}
 }
