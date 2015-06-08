16b363d92189290677431c0ca041578ab35bc83f
el-mubarok <mubarok13@gmail.com>
2015-06-09 05:59:24 +0700

    Fix

--- TB/src/Two.java
+++ TB/src/Two.java
@@ -10,7 +10,6 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JTree;
 
 
 public class Two extends JFrame {
@@ -21,7 +20,9 @@ public class Two extends JFrame {
 	private JTextField textField_2;
 	private JTextField textField_3;
 	private JTextField textField_4;
-	kondisiKelas data = new kondisiKelas();
+	
+	private kondisiKelas in2;
+
 	/**
 	 * Launch the application.
 	 */
@@ -42,38 +43,45 @@ public class Two extends JFrame {
 	 * Create the frame.
 	 */
 	public Two() {
-		setTitle("kondisiKelas");
+		setTitle("Kondisi Ruang Kelas");
 		setFont(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 264);
+		setBounds(100, 100, 450, 239);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnNext = new JButton("Next");
-		btnNext.setBounds(335, 191, 89, 23);
+		btnNext.setBounds(335, 165, 89, 23);
 		contentPane.add(btnNext);
 		btnNext.addActionListener(new ActionListener() {
-			
 			public void actionPerformed(ActionEvent e) {
+				in2 = new kondisiKelas();
+				
+				in2.setPanjangruang(Integer.parseInt(textField.getText()));
+				in2.setLebarruang(Integer.parseInt(textField_1.getText()));
+				in2.setJumlahkursi(Integer.parseInt(textField_2.getText()));
+				
+				classBaca.write1_3(in2);
 				new Thrre().setVisible(true);
-				data.setPanjangruang(Integer.parseInt(textField.getText()));
-				data.setLebarruang(Integer.parseInt(textField.getText()));
-				data.setJumlahkursi(Integer.parseInt(textField.getText()));
-				data.setJumlahpintu(Integer.parseInt(textField.getText()));
-				data.setJumlahjendela(Integer.parseInt(textField.getText()));
 				dispose();
 			}
-		});;
+		});
 		
+		JButton btnLoad = new JButton("Load");
+		btnNext.setBounds(335, 165, 89, 23);
+		contentPane.add(btnLoad);
+		btnLoad.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
 		
 		JButton btnPrev = new JButton("Prev");
-		btnPrev.setBounds(10, 191, 89, 23);
+		btnPrev.setBounds(10, 165, 89, 23);
 		contentPane.add(btnPrev);
 		btnPrev.addActionListener(new ActionListener() {
-			
-			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				new one().setVisible(true);
 				dispose();
@@ -87,47 +95,47 @@ public class Two extends JFrame {
 		contentPane.add(lblPanjangKelas);
 		
 		JLabel lblNewLabel = new JLabel("Lebar Ruang");
-		lblNewLabel.setBounds(72, 46, 89, 14);
+		lblNewLabel.setBounds(72, 36, 89, 14);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Jumlah Kursi");
-		lblNewLabel_1.setBounds(72, 79, 89, 14);
+		lblNewLabel_1.setBounds(72, 61, 89, 14);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Jumlah Pintu");
-		lblNewLabel_2.setBounds(72, 112, 89, 14);
+		lblNewLabel_2.setBounds(72, 86, 89, 14);
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		contentPane.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Jumlah Jendela");
-		lblNewLabel_3.setBounds(72, 143, 89, 14);
+		lblNewLabel_3.setBounds(72, 111, 89, 14);
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		contentPane.add(lblNewLabel_3);
 		
 		textField = new JTextField();
-		textField.setBounds(205, 9, 66, 26);
+		textField.setBounds(205, 9, 45, 16);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(205, 46, 66, 22);
+		textField_1.setBounds(205, 34, 45, 16);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(205, 79, 66, 22);
+		textField_2.setBounds(205, 61, 45, 14);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(205, 112, 66, 21);
+		textField_3.setBounds(205, 84, 45, 14);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
 		
 		textField_4 = new JTextField();
-		textField_4.setBounds(205, 144, 66, 23);
+		textField_4.setBounds(205, 109, 45, 14);
 		contentPane.add(textField_4);
 		textField_4.setColumns(10);
 	}
